46af910deb790c357952999248772d3a8897c689
Julio Miranda Jr <julioamj06@gmail.com>
2024-11-30 23:39:08 -0300

    Enviando o Restante dos Arquivos

---
+++ src/componentes/RodaPe/index.js
@@ -0,0 +1,11 @@
+import './RodaPe.css'
+
+const RodaPe = () => {
+    return(
+        <header className='rodape'>
+            <img src="/imagens/add.png" alt='O rodapé da pagina organo' />
+        </header>
+    )
+}
+
+export default RodaPe
